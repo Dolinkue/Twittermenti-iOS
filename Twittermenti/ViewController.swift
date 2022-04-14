d7b4a5e4786bbee2060780895fb97b001a236ba7
dolinkue <dolinkue_n@me.com>
2022-04-14 11:51:32 -0300

    probando api con el coreML

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,16 +7,39 @@
 //
 
 import UIKit
+import SwifteriOS
+import CoreML
+
+
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClassifier = MyTwitClassifier_1()
 
+    let swifter = Swifter(consumerKey: "rZfsfylmrVZB1CzYgYLEmRq5g", consumerSecret: "RWpgL2zpFiQWDXYDhH4buwC1Y0OGgxLLE6pmdBB5876NFT3TZz")
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+            // pasamos el metodo de prediccion que viene de mytwitclassifier
+      let prediction =   try! sentimentClassifier.prediction(text: "@Apple is terrible")
+        
+        //lo pasamos a string
+         print(prediction.label)
+        
+        //para buscar los twit desde swifter
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results,metadata) in
+            //print(results)
+        } failure: { error in
+            print("error api\(error)")
+        }
+
         
         
     }
