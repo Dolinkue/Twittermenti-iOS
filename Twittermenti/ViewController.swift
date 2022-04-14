02e2315e203d358bdc95f09291236d4d473113ad
dolinkue <dolinkue_n@me.com>
2022-04-14 16:13:13 -0300

    por para JSON, modelando la búsqueda para obtener el resultado

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SwifteriOS
 import CoreML
+import SwiftyJSON
 
 
 
@@ -27,15 +28,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            // pasamos el metodo de prediccion que viene de mytwitclassifier
-      let prediction =   try! sentimentClassifier.prediction(text: "@Apple is terrible")
-        
-        //lo pasamos a string
-         print(prediction.label)
-        
+
+
         //para buscar los twit desde swifter
-        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended) { (results,metadata) in
-            //print(results)
+        swifter.searchTweet(using: "#ukraine", lang: "en", count: 100, tweetMode: .extended) { (results,metadata) in
+            
+            
+            var tweets = [MyTwitClassifier_1Input]()
+            
+            // un for para cargar todos los tweest en un []
+            for i in 0..<100 {
+                
+                if let tweet = results[i]["full_text"].string {
+                    
+                    //esta let para cambiar el type de tweet ya que el input tiene que ser un inpuy y no un string
+                    let tweetForClassifier = MyTwitClassifier_1Input(text: tweet)
+                    
+                    tweets.append(tweetForClassifier)
+                    }
+                
+            }
+            
+            do {
+                
+              let predictions =  try self.sentimentClassifier.predictions(inputs: tweets)
+                
+                
+               var sentimentalScore = 0
+                
+                
+                for pred in predictions {
+                    
+                    let setiment = pred.label
+                    
+                    if setiment == "pos"{
+                    
+                        sentimentalScore += 1
+                    
+                    }else if setiment == "neg"{
+                    
+                        sentimentalScore -= 1
+                    
+                    }
+                    
+                            }
+                            
+                print(sentimentalScore)
+                
+            
+            }catch {
+                print("error\(error)")
+            }
+            
+            
+            
+            
+            
         } failure: { error in
             print("error api\(error)")
         }
